dc3e05f56607b340c2467c3cefecee9f4507610a
Julien Moutinho <julm@sourcephile.fr>
2026-08-30 00:41:32 +0200

    +buildability(haskell-os-kernel): init

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "haskell-os";
+  inputs = {
+    nixpkgs.url = "flake:nixpkgs";
+    cs140e = {
+      url = "github:dddrrreee/cs140e-25win";
+      flake = false;
+    };
+  };
+  outputs =
+    inputs:
+    let
+      inherit (inputs.nixpkgs) lib;
+      foreachSystem =
+        f:
+        lib.genAttrs lib.systems.flakeExposed (
+          system:
+          f rec {
+            inherit system;
+            pkgs = inputs.nixpkgs.legacyPackages.${system};
+          }
+        );
+    in
+    {
+      packages = foreachSystem (
+        { pkgs, system, ... }:
+        {
+          haskell-os-kernel = pkgs.stdenv.mkDerivation {
+            name = "haskell-os-kernel";
+            src =
+              with lib.fileset;
+              toSource {
+                root = ./.;
+                fileset = unions [
+                  ./Makefile
+                  ./asm
+                  ./c
+                  ./hs
+                  ./memmap
+                ];
+              };
+            nativeBuildInputs = [
+              # https://github.com/agniv-the-marker/haskell-os/blob/main/GUIDE.md
+              # requires MicroHs 0.15.0.0 or similar, this is 0.15.4.0
+              pkgs.microhs
+              #pkgs.targetPackages.haskell.packages.microhs.ghc
+              pkgs.pkgsCross.arm-embedded.buildPackages.gcc
+            ];
+            buildPhase = ''
+              set -x
+              make kernel.img \
+                PATH="$PATH" \
+                MHS_RUNTIME=${
+                  # Note that this does not include any patch from Nixpkgs
+                  pkgs.microhs.src
+                }/src/runtime
+            '';
+            installPhase = ''
+              cp -v kernel.img $out
+            '';
+          };
+        }
+      );
+    };
+}
